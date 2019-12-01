fb0318bcc4e89ad0f741cbc0eb4a2bb76e449f83
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-12-01 20:29:56 +0530

    moved routing logic to separate directory

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
-const express = require("express");
+const app = require("express")();
 const logger = require("morgan");
 const parsedBody = require("body-parser");
 const mongoose = require("mongoose");
-const List = require("./model/todo.model");
-
-const app = express();
-const router = express.Router();
+const router = require('./routes');
 
 const url = "mongodb://localhost:27017/Checklist";
 
@@ -17,48 +14,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
 app.use(logger("dev"));
 app.use(parsedBody.json());
-
-router.get("/", (req, res) => {
-  res.redirect('/lists');
-});
-
-router.get("/lists", (req, res) => {
-  List.find((err, lists) => {
-    if (err) return { success: false, error: err };
-    return res.json({ success: true, lists });
-  });
-});
-
-router.post("/addNote", (req, res) => {
-  let list = new List();
-  const { id, message } = req.body;
-  if (!id || !message) return res.status(404).json({ success: false, error: "Invalid inputs" });
-  list.id = id;
-  list.message = message;
-  list.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-router.delete("/deleteNote", (req, res) => {
-  const { id } = req.body;
-  List.findByIdAndDelete(id, (error, deleted) => {
-    if (error) return res.json({ success: false, error });
-    return res.json({ success: true, deleted });
-  });
-});
-
-router.put("/updateNote", (req, res) => {
-  const { id, update } = req.body;
-  if (!id || !update.message) return res.status(404).json({ success: false, error: "Invalid inputs" });
-  List.findByIdAndUpdate(id, update, (error, updated) => {
-    if (error) return res.json({ success: false, error });
-    return res.json({ success: true, updated });
-  });
-});
-
-app.use("/", router);
+app.use('/', router);
 
 app.listen(3001, () => {
   console.log("Server is up");
