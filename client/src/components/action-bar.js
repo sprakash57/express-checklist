0d88b39f9f91489797bdeeeb5a2c457884fba414
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-12-03 22:46:48 +0530

    addred skeleton for header and actionBar

---
+++ client/src/components/action-bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ActionBar = () => {
+    return (
+        <>
+            <button>Active</button>
+            <button>Done</button>
+        </>
+    )
+}
+
+export default ActionBar;
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './header';
+import ActionBar from './action-bar';
 
 const App = () => {
     return (
-        <div className='first'>Hello Welcome</div>
+        <>
+            <Header />
+            <ActionBar />
+        </>
     )
 }
 
