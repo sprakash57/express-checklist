a82ad266a279fced0e21c41526e21c4e9d20670b
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-04-25 20:33:33 +0530

    added initial setup for lambda

---
+++ libs/handler-lib.js
@@ -0,0 +1,20 @@
+export default function handler(lambda) {
+    return function (event, context) {
+        return Promise.resolve()
+            //Run the lambda
+            .then(() => lambda(event, context))
+            //on Success
+            .then(responseBody => [200, responseBody])
+            //on Failure
+            .catch(err => [500, { error: err.message }])
+            //Return HTTP response
+            .then(([statusCode, body]) => ({
+                statusCode,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                body: JSON.stringify(body)
+            }));
+    }
+}
